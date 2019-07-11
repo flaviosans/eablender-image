1b4964bf092f2ee3e40927fd52b3e61da272c6c4
Victor Gabriel <victorgabrieldecpaulino@gmail.com>
2019-07-11 15:36:37 -0300

    :sparkles: Implementa o card para coleção de ideias

--- card_image.php
+++ card_image.php
@@ -1,29 +1,43 @@
 <?php $plugin_path = plugin_dir_url( __FILE__ );
 
-$image = $response->content->productShowcase->userApp->userAppDetails;
-$productShowcaseImage = $response->content->path;
-$name = $response->content->productShowcase->userApp->name;
-
-if ($image->cover) {
-  $image = "https://aes-entenda-antes-teste-arquivos.s3.amazonaws.com/" .$image->cover;
+if (isset($response->content->albumProject)) {
+  $check_object = $response->content->albumProject;
 }
 
-else {
-  $image = $plugin_path ."avatar.jpeg";
+else if (isset($response->content->productShowcase)) {
+  $check_object = $response->content->productShowcase;
 }
+
+    $backgroundImage = $response->content->path;
+    $profile_picture = $check_object->userApp->userAppDetails;
+    $user_app = $check_object->userApp;
+
+    
+    if ($profile_picture->avatar) {
+      $profile_picture = "https://aes-entenda-antes-teste-arquivos.s3.amazonaws.com/" .$profile_picture->avatar;
+    }
+    
+    else {
+      $profile_picture = $plugin_path ."avatar.jpeg";
+    }
+    
+    //Adicionando o segmento
+    if ($user_app->segments) {
+      $segments = $user_app->segments[0]->name;
+    }
 
 ?>
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="card.css">
 
 <style>.user-img {
-  height: 70px;
-  width: 70px;
-  margin-top: 20px;
-  border-radius: 50px;
-  object-fit: cover;
+    height: 100px;
+    width: 100px;
+    margin-top: 20px;
+    border-radius: 50px;
+    object-fit: cover;
+    border: 2px solid #0579FF;
 }
 
 .productShowcase-img {
@@ -43,34 +57,39 @@ else {
 
 .budget-button {
   background-color: #ff7700;
+  margin: 15px 0px 15px 0px;
+}
+
+.shadow {
+  -webkit-box-shadow: 4px 7px 14px -6px rgba(0,0,0,0.75);
+-moz-box-shadow: 4px 7px 14px -6px rgba(0,0,0,0.75);
+box-shadow: 4px 7px 14px -6px rgba(0,0,0,0.75);
 }
 
 </style>
 
-<div>
+<div class="shadow">
   <div>
     <img class="productShowcase-img"
-      src="https://aes-entenda-antes-teste-arquivos.s3.amazonaws.com/<?php echo "$productShowcaseImage" ?>">
+      src="https://aes-entenda-antes-teste-arquivos.s3.amazonaws.com/<?php echo "$backgroundImage" ?>">
   </div>
 
-  <footer style="background-color: #0579FF;">
+  <footer style="background-color: #FFF;">
     <div class="row" style="align-items: center; padding-bottom: 10px;">
 
-      <div class="col-3" style="text-align: center;">
-        <img class="user-img" src="<?php echo "$image" ?>">
+      <div class="col-4" style="text-align: center;">
+        <img class="user-img" src="<?php echo "$profile_picture" ?>">
       </div>
 
-      <div class="col-9">
-        <h4 style="text-align: left; color: white; line-height: 0.9em;"><?php echo "$name" ?></h4>
-        <span style="text-align: left; color: white;">Engenheiro, decorador</span>
-        <div>
+      <div class="col-8">
+        <h3 style="text-align: left; color: #606062; line-height: 0.9em;"><?php echo "$user_app->name" ?></h3>
+        <span style="text-align: left; color: #606062;"><?php echo $segments?></span>
+        <br/>
+        <button onclick="window.location.href = 'https://app.entendaantes.com.br/site/<?php echo "$user_app->username"?>/home'"  type="button" class="btn budget-button">
+         Solicitar orçamento
+        </button>
+      </div>
 
-          <div class="col-12" style="text-align: right;">
-            <button type="button" class="btn btn-template budget-button">
-              <a href="https://app.entendaantes.com.br/peca-seu-orcamento">Solicitar orçamento</a>
-            </button>
-          </div>
-
-        </div>
+    </div>
   </footer>
 </div>
--- eablender-image.php
+++ eablender-image.php
@@ -18,32 +18,24 @@ class EABlender_image {
 	}
 
 	public function eablender_image($atts = []){
-		$value = shortcode_atts( ['id' => 'no-id'], $atts );
 		$return = null;
+		$value = shortcode_atts( ['id' => 'no-id'], $atts );
 		$response = $this->api->get_product_showcase_image( $value['id'] );
 
-		if(!$response->status_code == 200){
+		if ($response->status_code != 200) {
+			$response = $this->api->get_albuns_project_image( $value['id'] );
+		}
 
-			$content = $response->content;
-			$return  = "É essa a imagem que você queria?<p> <img src=\"";
-			$return .= "https://aes-entenda-antes-teste-arquivos.s3.amazonaws.com/";
-			$return .= $content->path;
-			$return .= "\"><br>";
-			$return .= 'Titulo da imagem: ' . $content->title . " ";
-			if(isset($content->keywords)){
-				$return .= 'Tags:';
-				foreach($content->meta->keywords as $keyword){
-					$return .= $keyword . '|';
-				}
-			}
-			$return .= "Título do álbum: " . $content->productShowcase->title;
-
-			$return .= "<br>Nome do profissional:" . $content->productShowcase->userApp->name;
+		if($response->status_code == 200){
+			ob_start();
+			include("card_image.php");
+			$return = ob_get_clean();
 		} else {
 			$return = $response->status_code . " " . $response->err_message;
 		}
 		return $return;
 	}
+
 }
 
 $eablender_image = new EABlender_image();
